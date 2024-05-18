64bda8c519d63ef40b9c85259baddd871533aec9
Kirill <k.gorbushin@gmail.com>
2024-05-18 23:39:45 +0200

    Base version
    Base version

---
+++ convert-mp4-to-text.py
@@ -0,0 +1,72 @@
+import os
+import ffmpeg
+import argparse
+
+INPUT_VIDEOS_PATH = 'input-videos'
+PROCESSED_PATH = 'processed'
+TEXT_PATH = 'text'
+
+def ffmpeg_convert_mp4_to_wav(file_name):
+    print("Converting to wav")
+    print("Input file: ", file_name)
+    filename_without_extension = file_name.split('.')[0]
+    print(filename_without_extension)
+    ffmpeg = f'ffmpeg -i {INPUT_VIDEOS_PATH}/{filename_without_extension}.mp4 -acodec pcm_s16le -ac 1 -ar 16000 {PROCESSED_PATH}/{filename_without_extension}-processed.wav'
+    os.system(ffmpeg)
+
+def ffmpeg_convert_mp4_to_mp3(file_name):
+    print("Converting to mp3")
+    print("Input file: ", file_name)
+    filename_without_extension = file_name.split('.')[0]
+    print(filename_without_extension)
+    ffmpeg = f'ffmpeg -i {INPUT_VIDEOS_PATH}/{filename_without_extension}.mp4 {PROCESSED_PATH}/{filename_without_extension}-processed.mp3'
+    os.system(ffmpeg)
+
+def whisper_transcribe_mp3(file_name, language):
+    #model = whisper.load_model("medium")
+    #result = model.transcribe(file_name, language=language, fp16=False)
+    filename_without_extension = file_name.split('.')[0]
+    print(filename_without_extension)
+    #with open(f'{filename_without_extension}.txt', 'w') as f:
+    #    f.write(result["text"])
+    whisper_command = 'Whisper-mac'
+    model_path = '-m ../whisper.cpp/models/ggml-small.bin'
+    output_options = f'-l {language} --output-txt --output-file {TEXT_PATH}/{filename_without_extension} {PROCESSED_PATH}/{filename_without_extension}-processed.wav'
+
+    whisper = f'{whisper_command} {model_path} {output_options}'    
+    os.system(whisper)
+
+
+def main():
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Convert mp4 to mp3 or to wav and then to text.")
+
+    # Create a mutually exclusive group for the commands
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--transcribe', action='store_true', help='Transcribe the mp3 file to text.')
+    group.add_argument('--convert', action='store_true', help='Convert only mp4 file to mp3/wav.')
+    group.add_argument('--convert-transcribe', action='store_true', help='Convert the file and transcribe the mp3 file to text.')
+
+    # Add the other arguments
+    parser.add_argument('filename', type=str, help='The name of the file to convert')
+    parser.add_argument('--mp3', action='store_true', help='Convert mp4 to mp3')
+    parser.add_argument('--wav', action='store_true', help='Convert mp4 to wav')
+    parser.add_argument('--language', type=str, default='en', help='Language for transcribing')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    if args.transcribe:
+        whisper_transcribe_mp3(args.filename, args.language)
+    elif args.convert:
+        if args.mp3:
+            ffmpeg_convert_mp4_to_mp3(args.filename)
+        elif args.wav:
+            ffmpeg_convert_mp4_to_wav(args.filename)        
+    elif args.convert_transcribe:
+        print("Converting and transcribing")
+        ffmpeg_convert_mp4_to_wav(args.filename)   
+        whisper_transcribe_mp3(args.filename, args.language)   
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,75 @@
+import time
+from flask import Flask, render_template, request, jsonify
+import os
+import subprocess
+from llama_cpp import Llama
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+PATH_TO_SAVE_VIDEO = "./downloaded/"
+CONVERTER_SCRIPT = "convert-mp4-to-text.py"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process_video', methods=['POST'])
+def process_video():
+    filename = request.form.get('filename')
+    language = request.form.get('language')
+    
+    path = filename + ".mp4"
+    print(path)
+    process = subprocess.Popen(['python', CONVERTER_SCRIPT, '--convert-transcribe', '--language', language, path])
+    process.wait()
+
+    txt_file_path = "text/" + filename + ".txt"
+
+    if os.path.exists(txt_file_path):
+        # Return the transcript as a JSON response
+        return jsonify({'transcript': open(txt_file_path).read()})
+    else:
+        return jsonify({'error': 'Error while processing, file not found.'})
+
+@app.route('/summarize_text', methods=['GET', 'POST'])
+def summarize_text():
+    #filename = request.form.get('filename')
+    start_time = time.time()
+    token_count = 0
+
+    filename = "2024-03-14-Sleep-Engineering"
+    llama = Llama(
+        model_path="../models/mistral-7b-instruct-v0.1.Q4_0.gguf",
+        n_gpu_layers=16, # Uncomment to use GPU acceleration
+      # seed=1337, # Uncomment to set a specific seed
+        n_ctx=4096, # Uncomment to increase the context window
+    )
+    text = open("text/" + filename + ".txt").read()
+
+    while True:
+        output = llama(
+            "Q: Summarize this text in just two paragraphs each 5-6 sentences long. " + text + " A: ",
+            max_tokens=None,
+            stop=["Q:", "\n"],
+            echo=True
+        )
+        generated_text = output['choices'][0]['text']
+        token_count += len(generated_text.split())
+        print(f"Progress: {token_count} tokens generated, {time.time() - start_time:.2f} seconds elapsed")
+        if "." in generated_text.strip().split()[-1]:
+            break
+
+    # Extract the generated text from the output dictionary, removing the initial prompt
+    generated_text = generated_text.split("A: ", 1)[1]
+
+    # Write the generated text to a file
+    summary_file_path = "summary/" + filename + ".txt"
+    with open(summary_file_path, 'w') as file:
+        file.write(generated_text)
+
+    return jsonify({'summary': generated_text})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
